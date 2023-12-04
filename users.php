4f550a87573f57b13ba3a7637b4e0c4c8998f500
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-12-04 17:21:06 +0800

    first commit

---
+++ users.php
@@ -0,0 +1,16 @@
+<?php 
+    include "headers.php";
+
+    class User{
+        
+    }
+
+    $json = isset($_POST["json"]) ? $_POST["json"] : "0";
+    $operation = isset($_POST["operation"]) ? $_POST["operation"] : "0";
+
+    $user = new User();
+
+    switch($operation){
+
+    }
+?>
