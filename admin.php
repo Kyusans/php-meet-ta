ff6ecbad04f1b8160327b303a8273431371a5084
Mel Macario <xmelmacario@gmail.com>
2023-12-08 13:16:36 +0800

    added addScholar function in admin.php

--- admin.php
+++ admin.php
@@ -19,6 +19,23 @@
             }
             return $returnValue;
         }
+
+        function addScholar($json){
+            include "connection.php";
+            $json = json_decode($json, true);
+            $sql = "INSERT INTO tbl_scholars(stud_school_Id, stud_last_name, stud_first_name, stud_course_id, stud_year_level, stud_scholarship_type_id, stud_password) 
+            VALUES(:schoolId, :lastName, :firstName, :courseId, :yearLevel, :scholarShipId, :password)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(":schoolId", $json["schoolId"]);
+            $stmt->bindParam(":lastName", $json["lastName"]);
+            $stmt->bindParam(":firstName", $json["firstName"]);
+            $stmt->bindParam(":courseId", $json["courseId"]);
+            $stmt->bindParam(":yearLevel", $json["yearLevel"]);
+            $stmt->bindParam(":scholarShipId", $json["scholarShipId"]);
+            $stmt->bindValue(":password", $json["lastName"] . $json["schoolId"]);
+            $stmt->execute();
+            return $stmt->rowCount() > 0 ? 1 : 0;
+        }
     }//admin 
 
     $json = isset($_POST["json"]) ? $_POST["json"] : "0";
@@ -29,6 +46,9 @@
     switch($operation){
         case "login":
             echo $admin->login($json);
+            break;
+        case "addScholar":
+            echo $admin->addScholar($json);
             break;
     }
 ?>
