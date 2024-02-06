8b3951f43db6d6c2272fa408631648f7af1d2ac5
Mel Macario <xmelmacario@gmail.com>
2024-02-06 10:22:38 +0800

    first commit

---
+++ api/user.php
@@ -0,0 +1,46 @@
+<?php
+include "headers.php";
+
+class User
+{
+  function signup($json)
+  {
+    include "connection.php";
+    $data = json_decode($json, true);
+    $image = "emptyImage.jpg";
+    $sql = "INSERT INTO tbl_users(user_username, user_email, user_password, user_image) VALUES(:username, :email, :password, :image)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":username", $data["username"]);
+    $stmt->bindParam(":email", $data["email"]);
+    $stmt->bindParam(":password", $data["password"]);
+    $stmt->bindParam(":image", $image);
+    $stmt->execute();
+    return $stmt->rowCount() > 0 ? 1 : 0;
+  }
+} //admin 
+
+function recordExists($value, $table, $column)
+{
+  include "connection.php";
+  $sql = "SELECT COUNT(*) FROM $table WHERE $column = :value";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(":value", $value);
+  $stmt->execute();
+  $count = $stmt->fetchColumn();
+  return $count > 0;
+}
+
+
+$json = isset($_POST["json"]) ? $_POST["json"] : "0";
+$operation = isset($_POST["operation"]) ? $_POST["operation"] : "0";
+
+$user = new User();
+
+switch ($operation) {
+    // case "login":
+    //     echo $user->login($json);
+    //     break;
+  case "signup":
+    echo $user->signup($json);
+    break;
+}
